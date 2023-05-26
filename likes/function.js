e4386e126beb092eeca08190fe980e1f5746bf43
Alexia Yearty <122060153+Ayearty@users.noreply.github.com>
2023-05-26 13:48:49 -0700

    Add files via upload

---
+++ likes/function.js
@@ -0,0 +1,22 @@
+var likes1 = 0;
+var likes1Element = document.querySelector("#likes1");
+function add1(){
+    likes1++;
+    likes1Element.innerText = likes1 + " like(s)";
+    console.log(likes1);
+}
+
+var likes2 = 0;
+var likes2Element = document.querySelector("#likes2");
+function add2(){
+    likes2++;
+    likes2Element.innerText = likes2 + " like(s)";
+    console.log(likes2);
+}
+var likes3 = 0;
+var likes3Element = document.querySelector("#likes3");
+function add3(){
+    likes3++;
+    likes3Element.innerText = likes3 + " like(s)";
+    console.log(likes3);
+}
